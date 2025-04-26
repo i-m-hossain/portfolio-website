4ff16ce546e543d566b6495301d03c0e487efe51
im.hossain <im.hossain@blamanagedservices.com>
2025-04-26 08:58:41 +0600

    tawk.to added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,20 @@
 'use client'
 
-import BackgroundAnimation from './BackgroundAnimation'
+
+import { useEffect } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import ParticleBackground from './ParticleBackground'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(()=>{
+    const script = document.createElement('script')
+    script.src = 'https://embed.tawk.to/680bc3a422d1d91910dd362f/1ipmv8f15'
+    script.async = true
+    script.charset = 'UTF-8'
+    script.setAttribute('crossorigin', '*')
+    document.body.appendChild(script)
+  },[])
   return (
     <div className="relative">
       <Header />
