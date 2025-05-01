a3ad6813a902a80e5e506c20f643c937bdd47d14
im.hossain <im.hossain@blamanagedservices.com>
2025-05-01 18:01:18 +0600

    footer dark theme updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white py-4 z-50">
+    <footer className="fixed bottom-0 left-0 right-0 bg-gray-200 text-dark dark:bg-gray-800 dark:text-white py-4 z-50 shadow-t">
       <div className="max-w-6xl mx-auto text-center">
         <p>&copy; {currentYear} Md Imran Hossain. All rights reserved.</p>
       </div>
